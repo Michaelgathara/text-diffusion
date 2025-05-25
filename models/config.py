ec87cc6d1425e7a92edc82edad418a47a955647a
michaelgathara <mikegtrm@gmail.com>
2025-05-25 13:14:41 -0500

    model: smaller model

--- models/config.py
+++ models/config.py
@@ -5,7 +5,7 @@ class ModelConfig:
         self.n_embd = 2304       # Embedding dimension (reduced from 2560)
         self.n_head = 32        # Number of attention heads 
         self.n_layer = 32      # Number of transformer layers 
-        self.block_size = 1024   # Max sequence length / context window (e.g., 128, 256, 512)
+        self.block_size = 768   # Max sequence length / context window (e.g., 128, 256, 512)
         self.dropout = 0.1      # Dropout rate
         
         # mem stuff
